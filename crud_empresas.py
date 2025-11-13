9140fffd80f12b1fc66b17b7f3250d2f1ae07641
PxS00 <lucas.rossonidd@gmail.com>
2025-11-13 18:22:52 -0300

    feat: Add CRUD functionality for empresas and usuarios

---
+++ crud_empresas.py
@@ -0,0 +1,32 @@
+# crud_empresas.py
+# CRUD para empresas
+
+ARQUIVO_EMPRESAS = 'empresas.json'
+
+
+def criar_empresa():
+    pass
+
+
+def listar_empresas():
+    pass
+
+
+def atualizar_empresa():
+    pass
+
+
+def deletar_empresa():
+    pass
+
+
+def buscar_empresa_por_id():
+    pass
+
+
+def buscar_empresa_por_nome():
+    pass
+
+
+def exportar_empresas_json():
+    pass
